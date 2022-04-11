da6abaf210605051ea7385c7eaeeaf16fecbcc14
XiaoPan <2489964425@qq.com>
2022-04-11 22:27:53 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      123
+      {/*// <BrowserRouter>*/}
+      {/*//   <Routes>*/}
+      {/*//     <Route path="/" element={<App />}>*/}
+      {/*<Route index element={<Home />} />*/}
+      {/*<Route path="teams" element={<Teams />}>*/}
+      {/*  <Route path=":teamId" element={<Team />} />*/}
+      {/*  <Route path="new" element={<NewTeamForm />} />*/}
+      {/*  <Route index element={<LeagueStandings />} />*/}
+      {/*</Route>*/}
+      {/*    </Route>*/}
+      {/*  </Routes>*/}
+      {/*</BrowserRouter>*/}
     </div>
   );
 }
