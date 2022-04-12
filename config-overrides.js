2cd984979c826e6adf71dbfb67636a7c3d0dd330
XiaoPan <2489964425@qq.com>
2022-04-12 11:17:10 +0800

    使用 react-app-rewired 修改配置, 配置 svg

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const {override, addWebpackModuleRule} = require('customize-cra');
+const path = require('path');
+
+module.exports = override(
+  addWebpackModuleRule({
+      test: /\.svg$/,
+      include: [path.resolve(__dirname, "./src/icons")],
+      use: [
+          {
+              loader: 'svg-sprite-loader',
+              options: {symbolId: "icon-[name]"}
+          }]
+  })
+);
